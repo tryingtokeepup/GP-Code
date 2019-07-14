55e17feeaae09f871cc876539ca59a485278921c
tryingtokeepup <kl.hawaii@gmail.com>
2019-07-14 14:33:25 -0700

    added header

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Popular from './components/Popular';
 // Components => we build the app by stringing components together
 // Composed of the following three things:
 // State
 // Lifecycle => fetching data from api, adding component to DOM
 // UI
 
-function isAuthed() {
-  return true;
-}
-function showWarning() {
-  return true;
-}
 export default class App extends React.Component {
   render() {
-    const authed = isAuthed();
-
-    const name = 'Kai';
-    // this is the description of the UI
-
     return (
-      <div>
-        <h1>📱</h1>
-        {showWarning() === true && <h3>😤</h3>}
+      <div classname="container">
+        <Popular />
       </div>
     );
   }
