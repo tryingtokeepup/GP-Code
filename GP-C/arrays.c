a51b8549720cbdc8f2b2edec0ef5590a6a855ba8
tryingtokeepup <kl.hawaii@gmail.com>
2019-07-24 22:00:02 -0700

    whoo, finished the arrays. now to hash.

--- GP-C/arrays.c
+++ GP-C/arrays.c
@@ -25,6 +25,25 @@ Array *create_array(int capacity)
     return arr;
 }
 
+void resize_array(Array *arr)
+{
+
+    int new_capacity = arr->capacity * 2;
+    char **new_elements = malloc(new_capacity * sizeof(char *));
+
+    for (int i = 0; i < arr->count; i++)
+    {
+
+        new_elements[i] = arr->elements[i];
+    }
+
+    // free old element array
+    free(arr->elements); // we don't free the individual elements because we still need access to them
+    // the new->elements[i] is the same pointer as arr->elements[i]
+    arr->elements = new_elements;
+    arr->capacity = new_capacity;
+}
+
 void destroy_array(Array *arr)
 {
 
@@ -42,12 +61,10 @@ void destroy_array(Array *arr)
 void arr_append(Array *arr, char *element)
 {
 
-    if (arr->capacity <= arr->count)
+    if (arr->capacity == arr->count)
     {
-
-        fprintf(stderr, "Yo, INDEXERRORRRRR!");
-        return;
-    };
+        resize_array(arr);
+    }
 
     // otherwise, let's copy elements and append to the back of the array
     // strdup is recommended => hidden malloc inside here
@@ -71,26 +88,115 @@ char *arr_read(Array *arr, int index)
     return arr->elements[index];
 }
 
+void arr_insert(Array *arr, char *element, int index)
+{
+
+    if (index > arr->count)
+    {
+        // modify this to call the resize function once we write it up!
+        fprintf(stderr, "IndexError: Index %d out of range! \n", index);
+        return;
+    }
+
+    // resize array if number of elements is over capacity
+    if (arr->capacity <= arr->count)
+    {
+
+        resize_array(arr);
+    }
+    // move every element after the insert index to the right (by one position)
+    for (int i = arr->count; i > index; i--)
+    {
+        arr->elements[i] = arr->elements[i - 1];
+    }
+    // copy the element over and add to array (use strdup)
+    char *new_element = strdup(element);
+    arr->elements[index] = new_element;
+    arr->count++;
+}
+
+void arr_remove(Array *arr, char *element)
+{
+    int removed = 0;
+    for (int i = 0; i < arr->count; i++)
+    {
+        if (removed)
+        {
+            arr->elements[i - 1] = arr->elements[i];
+        }
+        else if (strcmp(element, arr->elements[i]) == 0)
+        {
+            free(arr->elements[i]);
+            removed = 1;
+        }
+    }
+    if (removed)
+    {
+
+        arr->count--;
+    }
+    else
+    {
+        fprintf(stderr, "ValueError: %s is not in the array\n", element);
+    }
+}
+/*****
+ * Utility function to print an array.
+ *****/
+void arr_print(Array *arr)
+{
+    printf("[");
+    for (int i = 0; i < arr->count; i++)
+    {
+        printf("%s", arr->elements[i]);
+        if (i != arr->count - 1)
+        {
+            printf(",");
+        }
+    }
+    printf("]\n");
+}
+
 #ifndef TESTING
 int main(void)
 {
 
-    Array *arr = create_array(4);
+    Array *arr = create_array(1);
 
-    //arr_insert(arr, "STRING1", 0);
-    arr_append(arr, "STRING1");
-    arr_read(arr, 0);
+    arr_insert(arr, "STRING1", 0);
+    arr_append(arr, "STRING4");
+    arr_print(arr);
 
-    // printf, fprintf, sprintf
-    fprintf(stdout, "yo guys, it toally works now lol: %s\n", arr_read(arr, 0));
-    //   arr_insert(arr, "STRING2", 0);
-    //   arr_insert(arr, "STRING3", 1);
-    //   arr_print(arr);
-    //   arr_remove(arr, "STRING3");
-    //   arr_print(arr);
+    arr_insert(arr, "STRING2", 0);
+    arr_insert(arr, "STRING3", 1);
+    arr_print(arr);
 
+    arr_remove(arr, "STRING3");
+    arr_print(arr);
     destroy_array(arr);
 
     return 0;
 }
 #endif
+
+// int main(void)
+// {
+
+//     Array *arr = create_array(4);
+
+//     //arr_insert(arr, "STRING1", 0);
+//     arr_append(arr, "STRING1");
+//     arr_read(arr, 0);
+
+//     // printf, fprintf, sprintf
+//     fprintf(stdout, "yo guys, it toally works now lol: %s\n", arr_read(arr, 0));
+//     //   arr_insert(arr, "STRING2", 0);
+//     //   arr_insert(arr, "STRING3", 1);
+//     //   arr_print(arr);
+//     //   arr_remove(arr, "STRING3");
+//     //   arr_print(arr);
+
+//     destroy_array(arr);
+
+//     return 0;
+// }
